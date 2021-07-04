2e5ed7104dac56ce6a44cabdc893da7891cf8737
Cody Wilson <cody@codifynw.com>
2021-07-04 13:07:06 -0700

    auto prefix

--- component-app/rollup.config.js
+++ component-app/rollup.config.js
@@ -1,8 +1,7 @@
-import serve from 'rollup-plugin-serve'
-import livereload from 'rollup-plugin-livereload'
 import babel from '@rollup/plugin-babel'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import autoprefixer from 'autoprefixer'
 import replace from '@rollup/plugin-replace'
 import postcss from 'rollup-plugin-postcss'
 import path from 'path'
@@ -25,18 +24,11 @@ export default {
       presets: ['@babel/preset-react'],
     }),
     commonjs(),
-    // serve({
-    //   open: true,
-    //   verbose: true,
-    //   contentBase: ['', 'public'],
-    //   host: 'localhost',
-    //   port: 3000,
-    // }),
-    // livereload({ watch: 'dist' }),
     postcss({
-      extract: true,
-      // Or with custom file name
+      plugins: [autoprefixer()],
+      extensions: ['.sass', '.scss', '.css'],
       extract: path.resolve('dist/styles.css'),
+      minimize: true,
     }),
   ],
 }
