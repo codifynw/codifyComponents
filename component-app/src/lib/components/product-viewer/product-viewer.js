1c24275bace4390e16618a1710c134f4d094dd57
Cody Wilson <cody@codifynw.com>
2021-07-04 15:59:10 -0700

    implement simple useEffect and useHook

--- component-app/src/lib/components/product-viewer/product-viewer.js
+++ component-app/src/lib/components/product-viewer/product-viewer.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './product-viewer.scss'
 
 /**
  * Product Page
  */
 export const ProductViewer = ({ product, ...props }) => {
+  const [windowWidth, setwindowWidth] = useState(window.innerWidth)
+  const [selectedProduct, setselectedProduct] = useState(0)
+
+  const handleResize = () => {
+    setwindowWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize)
+  }, [])
+
+  useEffect(() => {
+    console.log('in selectedProduct use effect')
+    console.log('product', product)
+  }, selectedProduct)
+
+  let theFunction = function () {
+    console.log('test')
+  }
+
   return (
     <div className={['product-viewer'].join(' ')} {...props}>
-      <div>This is a div</div>
+      <div>{windowWidth}</div>
+      {product.options.map((option, index) => (
+        <button onClick={theFunction}>{option}</button>
+      ))}
     </div>
   )
 }
