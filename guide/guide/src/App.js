193eac7e6290f11760163b1af2c9255d08dacd82
codifynw <cody@codifynw.com>
2023-10-03 17:18:22 -0700

    wip js

--- guide/guide/src/App.js
+++ guide/guide/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+const generateCircularProgress = (percentage) => {
+  if (percentage < 0 || percentage > 100) {
+    return
+  }
+  const radius = 50
+  const circumference = 2 * Math.PI * radius
+  const offset = circumference - (percentage / 100) * circumference
+  return `radial-gradient(circle at center, #f5f5f5 50%, transparent 50%), radial-gradient(circle at center, #f5f5f5 50%, #f5f5f5 50%)`
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="journey">
+        {/* <div className="circle-border" style={{ background: generateCircularProgress(10) }}>
+          <div className="journey-progress-indicator"></div>
+        </div> */}
+        <div className="pie animate no-round" style={{'--p': 20}} />
+        <h1>Guide One</h1>
+        <p>Guide</p>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+
+
+
